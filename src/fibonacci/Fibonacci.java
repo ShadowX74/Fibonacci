015edce669185b44bd7f2c161cab401dd64bcbc4
ShadowX <ShadowX@ninja27.d22.lan>
2016-11-15 14:17:08 -0600

    Fibonacci.

---
+++ src/fibonacci/Fibonacci.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonacci;
+
+/**
+ *
+ * @author ShadowX
+ */
+public class Fibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i < 30; i++) {
+            System.out.println(i + 1 + ": " + fibonacci(i));
+        }
+    }
+    
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 2) + fibonacci(n - 1);
+        }
+    }
+    
+}
